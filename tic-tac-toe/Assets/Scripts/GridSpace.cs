55d034014d2a7c68f965f84420f541ed2833f9b1
Adam Joyce <adamjoyce@outlook.com>
2017-06-11 00:56:51 +0100

    Basic button selection and transition implemented

---
+++ tic-tac-toe/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;           // This object's UI button component.
+    public Text buttonText;         // The button's associated text component.
+    public string playerSide;       // 'X' or '0'.
+
+    // Called when the button is clicked.
+    public void SetSpace()
+    {
+        buttonText.text = playerSide;
+        button.interactable = false;
+    }
+}
