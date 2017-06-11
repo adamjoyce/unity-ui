c092e90bb13c88ef6477d2f1d312709d6314d820
Adam Joyce <adamjoyce@outlook.com>
2017-06-11 10:59:56 +0100

    Implemented core turn logic

---
+++ tic-tac-toe/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+
+    private void Awake()
+    {
+        setGameControllerReferenceOnButtons();
+    }
+
+    // Returns which player go it is.
+    public string getPlayerSide()
+    {
+        return "?";
+    }
+
+    // Ends the current player's turn.
+    public void endTurn()
+    {
+        Debug.Log("EndTurn is not implemented yet.");
+    }
+
+    // Sets up the game controller reference for all button in the grid space.
+    private void setGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; ++i)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().setGameController(this);
+        }
+    }
+}
--- tic-tac-toe/Assets/Scripts/GridSpace.cs
+++ tic-tac-toe/Assets/Scripts/GridSpace.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridSpace : MonoBehaviour
 {
-    public Button button;           // This object's UI button component.
-    public Text buttonText;         // The button's associated text component.
-    public string playerSide;       // 'X' or '0'.
+    public Button button;                           // This object's UI button component.
+    public Text buttonText;                         // The button's associated text component.
+
+    private GameController gameController;          // The scene's game controller.
 
     // Called when the button is clicked.
-    public void SetSpace()
+    public void setSpace()
     {
-        buttonText.text = playerSide;
+        buttonText.text = gameController.getPlayerSide();
         button.interactable = false;
+        gameController.endTurn();
+    }
+
+    // Sets the scene's gamecontroller for this button.
+    public void setGameController(GameController controller)
+    {
+        gameController = controller;
     }
 }
