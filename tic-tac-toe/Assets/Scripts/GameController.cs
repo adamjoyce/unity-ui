6acff15828c78adae5da39341b3ea5fbdcafd04d
Adam Joyce <adamjoyce@outlook.com>
2017-06-11 15:10:58 +0100

    Added restart logic and button

--- tic-tac-toe/Assets/Scripts/GameController.cs
+++ tic-tac-toe/Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ public class GameController : MonoBehaviour
     public Text[] buttonList;
     public GameObject gameOverPanel;
     public Text gameOverText;
+    public GameObject restartButton;
 
     private string playerSide;
     private int moveCount;
@@ -17,6 +18,7 @@ public class GameController : MonoBehaviour
     {
         setGameControllerReferenceOnButtons();
         gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
         playerSide = "X";
         moveCount = 0;
         maxMoves = 9;
@@ -34,59 +36,73 @@ public class GameController : MonoBehaviour
         // Top row.
         if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Middle row.
         if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Bottom row.
         if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Left column.
         if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Middle column,
         if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Right column.
         if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Top-left diagonal.
         if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Top-right diagonal.
         if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
         {
-            gameOver();
+            gameOver(playerSide);
         }
 
         // Check for a draw.
         moveCount++;
         if (moveCount >= maxMoves)
         {
-            setGameOverText("It's a draw!");
+            gameOver("draw");
         }
 
         changeSides();
+    }
+
+    // Resets the game for another round.
+    public void restartGame()
+    {
+        playerSide = "X";
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
+        setBoardInteractable(true);
+        for (int i = 0; i < buttonList.Length; ++i)
+        {
+            buttonList[i].text = "";
+        }
     }
 
     // Sets up the game controller reference for all button in the grid space.
@@ -99,14 +115,21 @@ public class GameController : MonoBehaviour
     }
 
     // Deals with cleanup and ending the game.
-    private void gameOver()
+    private void gameOver(string winningPlayer)
     {
-        for (int i = 0; i < buttonList.Length; ++i)
+        setBoardInteractable(false);
+
+        // Displays the if the game was a win or a draw.
+        if (winningPlayer == "draw")
         {
-            buttonList[i].GetComponentInParent<Button>().interactable = false;
+            setGameOverText("It's a draw!");
+        }
+        else
+        {
+            setGameOverText(playerSide + " Wins!");
         }
 
-        setGameOverText(playerSide + " Wins!");
+        restartButton.SetActive(true);
     }
 
     // Swaps the player to the other side.
@@ -120,5 +143,14 @@ public class GameController : MonoBehaviour
     {
         gameOverText.text = text;
         gameOverPanel.SetActive(true);
+    }
+
+    // Toggles the board's button interactibility.
+    private void setBoardInteractable(bool toggle)
+    {
+        for (int i = 0; i < buttonList.Length; ++i)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = toggle;
+        }
     }
 }
