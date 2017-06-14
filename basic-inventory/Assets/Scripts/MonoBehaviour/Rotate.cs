e4842ff5fa9518a80a1065c973ac98fd872dbba4
Adam Joyce <adamjoyce@outlook.com>
2017-06-14 12:25:08 +0100

    Added simple rotation script to items

---
+++ basic-inventory/Assets/Scripts/MonoBehaviour/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationSpeed = 100.0f;                     // The speed at which the object will rotate.
+
+    /* Update is called once per frame. */
+    void Update()
+    {
+        transform.Rotate((Vector3.up * rotationSpeed) * Time.deltaTime);
+    }
+}
